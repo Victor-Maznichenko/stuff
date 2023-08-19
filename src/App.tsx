1b0bc263a14ba4b2c435ac4d035e5bf8fffdcb51
Victor Maznichenko <victor_maznichenko@mail.ru>
2023-08-19 08:44:23 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import Footer from "./components/Footer/Footer"
+import Header from "./components/Header/Header"
+import AppRoutes from "./components/Routes/Routes"
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <AppRoutes />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
